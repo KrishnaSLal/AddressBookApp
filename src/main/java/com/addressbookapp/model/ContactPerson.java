8ad0fd14ba29a32b1ddcdf26bf4139c6d021b338
KrishnaSLal <krishnaslal0204@gmail.com>
2026-03-09 11:12:49 +0530

    [KrishnaSLal]: Modified the getter and setter method

--- src/main/java/com/addressbookapp/model/ContactPerson.java
+++ src/main/java/com/addressbookapp/model/ContactPerson.java
@@ -1,14 +1,8 @@
 package com.addressbookapp.model;
 
-import jakarta.persistence.*;
-
-@Entity
 public class ContactPerson {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+	
+	private int id;
     private String firstName;
     private String lastName;
     private String address;
@@ -18,7 +12,8 @@ public class ContactPerson {
     private String phoneNumber;
     private String email;
 
-    public ContactPerson() {}
+    public ContactPerson() {
+    }
 
     public ContactPerson(String firstName, String lastName, String address,
                          String city, String state, String zip,
@@ -32,34 +27,88 @@ public class ContactPerson {
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+    public ContactPerson(int id, String firstName, String lastName, String address,
+            String city, String state, String zip,
+            String phoneNumber, String email) {
+    	this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    
+    public int getId() {
+    	return id;
+    }
+    public void setId() {
+    	this.id = id;
+    }
 
-    public ContactPerson(String string, String string2, String string3, String string4, String string5, String string6,
-			String string7) {
-		// TODO Auto-generated constructor stub
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() {
+        return lastName;
+    }
 
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
+    public String getAddress() {
+        return address;
+    }
 
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-    public String getState() { return state; }
-    public void setState(String state) { this.state = state; }
+    public String getCity() {
+        return city;
+    }
 
-    public String getZip() { return zip; }
-    public void setZip(String zip) { this.zip = zip; }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getState() {
+        return state;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
