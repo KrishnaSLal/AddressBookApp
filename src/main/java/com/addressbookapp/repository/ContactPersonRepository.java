1c241d03c42f90babbf0144b6d36d7de7c620d19
KrishnaSLal <krishnaslal0204@gmail.com>
2026-03-09 11:02:21 +0530

    [KrishnaSLal]: Added UC17 update contact and sync with db

---
+++ src/main/java/com/addressbookapp/repository/ContactPersonRepository.java
@@ -0,0 +1,8 @@
+
+package com.addressbookapp.repository;
+
+import com.addressbookapp.model.ContactPerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactPersonRepository extends JpaRepository<ContactPerson, Long> {
+}
